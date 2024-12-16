0e752df7a56674ee73b4e15f5bc0a2cd4f31b270
Tomas Stuikys <tomas.stui@gmail.com>
2024-12-16 13:31:47 +0000

    Added interfaces.py, now the script will be able to create directories with the name of today

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from interfaces import *
+from outputs import *
+from datetime import date
 
 if __name__ == "__main__":
     interfaces = get_tshark_interfaces()
     interface_found = check_for_nrf_sniffer(interfaces)
+
+
     if interface_found:
         print("nRF Sniffer for Bluetooth LE was found!")
+        today = date.today().strftime("%d-%m-%Y")
+        directory = f"outputs\\{today}"
+        ensure_directory_exists(directory)
+        ## Check if outputs directory exists
+        ## Make outputs directory
+        ## Check if Todays date directory exists
+        ## Make "Todays date directory"
     else:
         print("nRF Sniffer for Bluetooth LE was not found!")
+        print("Exiting program!")
+        exit()
---
+++ outputs.py
@@ -0,0 +1,27 @@
+import os
+from datetime import date
+
+def ensure_directory_exists(name_of_directory: str):
+    """
+        ensure_directory_exists function checks if a directory with provided name
+        exists. If it doesn't it will be created.
+
+        :param name_of_directory: the name of the directory to be checked/created 
+    """    
+    # Check if the directory exists
+    if not os.path.exists(name_of_directory):
+        # Create the directory
+        os.makedirs(name_of_directory)
+        print(f"Folder '{name_of_directory}' created.")
+    else:
+        print(f"Folder '{name_of_directory}' already exists.")
+
+def create_todays_directory():
+    """
+        create_todays_directory - gets todays datetime in dd-mm-yyyy
+        formate and creates a directory in outputs directory with the same name.
+    """
+
+    today = date.today().strftime("%d-%m-%Y")
+    directory = f"outputs\\{today}"
+    ensure_directory_exists(directory)
