c4dd15ff96f125fb6c8d872d2aa79de1fc5a8d23
Striker <tomas.stui@gmail.com>
2025-03-06 09:27:08 +0000

    Removing pycache

--- back_end/server/config.py
+++ back_end/server/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "devices.db")}'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "outputs/devices.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
