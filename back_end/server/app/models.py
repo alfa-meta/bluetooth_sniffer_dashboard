cf498ca6c86291b7837de65494b36b998280d858
Striker <tomas.stui@gmail.com>
2025-03-07 21:49:35 +0000

    Removed unique on username, adapted config.py to work better and work on Windows

--- back_end/server/app/models.py
+++ back_end/server/app/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'USER'
     uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     devices = db.relationship('Device', backref='user', lazy=True)
--- back_end/server/config.py
+++ back_end/server/config.py
@@ -2,7 +2,13 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "outputs/devices.db")}'
+    db_dir = os.path.join(basedir, "outputs")
+    db_path = os.path.join(db_dir, "devices.db")
+
+    # Ensure directory exists
+    os.makedirs(db_dir, exist_ok=True)
+
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     PASSWORD_SALT = os.getenv("PASSWORD_SALT")
